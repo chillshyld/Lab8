4d57d1c2f334e6384ffcd3886aa119789658e120
chillshyld <chill_shyld@hotmail.com>
2016-03-04 18:04:37 +0700

    change 5

--- Lab8(3)/Lab8_3_.py
+++ Lab8(3)/Lab8_3_.py
@@ -6,6 +6,11 @@ def main():
     app = QtAPPlication(sys.argv)
     w = Simple_drawing_window()
     w.show()
-    print("hey")
+ 
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QImage("images/rabbit.png")
 
 
